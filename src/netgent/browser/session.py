d6708b1dd98c1918ef069925ef23c4148fec083b
JulianKrauze <150268679+JulianKrauze@users.noreply.github.com>
2026-07-14 15:34:46 -0400

    Fixed typo in previos push

--- src/netgent/browser/session.py
+++ src/netgent/browser/session.py
@@ -27,7 +27,6 @@ class BrowserSession:
         os.environ["SSLKEYLOGFILE"] = "/capture/tar_dir/sslkeylog.log"
         if user_data_dir:
             self._default_args.append(f" --user-data-dir={user_data_dir}")
-        import os
         net_log_path = os.environ.get('NETGENT_NET_LOG')
         if net_log_path:
             self._default_args.append(f"--log-net-log={net_log_path}")
